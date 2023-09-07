3a62dc3e243b9be260a2265fe38f14cc3fe1214e
jadonus <jadongearhart@gmail.com>
2023-09-07 15:52:58 -0400

    done

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -9,9 +9,9 @@
       bootstrap: `
         First of all, this is my opinion. Someone may think differently than me, and that is ok. Bootstrap is amazing for new web developers. It looks good out of the box. Check. It is Extremely good at making responsize websites. Check. It is accesable for everyone, with amazing documention and a massive userbase. Check. But here is where it goes wrong. Bootstrap is simply not customizable. Their color pallete is good, and with the semi recent release of Bootstrap 5, it has gotten better, but it does not have the flexibility like regular css. Don't get me wrong, I have some things to say about that too, but in regards to color, that is where regular css wins. In my opinion, what this website is built on uses what is the perfect balence between the two: Tailwind CSS. Tailwind still has a limited color pallete, but much less than Bootstrap. Tailwind's downside is no prebuilt items, like spinners, cards, buttons, etc. Instead of using the prebuilt items provided by Bootstrap, you get to make your own. This allows for flexibility and creativity at the expense of speed. I think that at the end of the day, Boostrap has its strengths, but Tailwind is my favorite. For now....
       `,
-      bootstrap20: `
-        First of all, this is my opinion. Someone may think differently than me, and that is ok.
-        // Rest of your blog content
+      hello: `
+Hello. I dont know why I wrote this, but still, Hello. 
+By the way, Hello.
       `
     };
   
@@ -28,18 +28,23 @@
       }
     });
   </script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link href="https://fonts.cdnfonts.com/css/atakana-sarif" rel="stylesheet">
  <body class=" dark:bg-black h-screen dark:text-white antialiased">
   <!-- The rest of your HTML -->
   <main class="animate-fade-right p-4 sm:p-8 md:p-12 lg:p-16 xl:p-20">
     <h1 class="m-6 title text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl"> {$page.params.slug}</h1>
-    <div class="mt-4 text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-3xl">
+    <div class="hello mt-4 text-xl sm:text-xl md:text-xl lg:text-2xl xl:text-3xl">
       {$currentBlog}
     </div>
   
   </main>
   </body>
 <style>
-        main {
+        .hello {
             font-family: 'Poppins', sans-serif;
         }
 
